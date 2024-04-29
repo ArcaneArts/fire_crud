fb1a37e4a603223e30c1e29c5cd3c2884e43f133
cyberpwn <danielmillst@gmail.com>
2024-04-29 04:19:47 -0400

    Slivers

--- lib/src/fire_list.dart
+++ lib/src/fire_list.dart
@@ -135,3 +135,99 @@ class _FireListState<T> extends State<FireList<T>> {
                       : widget.builder(context, item),
                   loading: widget.loading))));
 }
+
+int _kDefaultSemanticIndexCallback(Widget _, int localIndex) => localIndex;
+
+class FireSliverList<T> extends StatefulWidget {
+  final FireCrud<T> crud;
+  final QueryBuilder? query;
+  final int streamWindow;
+  final int streamWindowPadding;
+  final int memorySize;
+  final Duration streamRetargetCooldown;
+  final Duration sizeCheckInterval;
+  final int limitedSizeDoubleCheckCountThreshold;
+  final Widget Function(BuildContext context, T data) builder;
+  final Widget loading;
+  final Widget failed;
+  final Widget empty;
+  final Function(CollectionViewer<T>)? onViewerInit;
+  final SemanticIndexCallback semanticIndexCallback;
+  final int semanticIndexOffset;
+  final bool addAutomaticKeepAlives;
+  final bool addRepaintBoundaries;
+  final bool addSemanticIndexes;
+  final int? semanticChildCount;
+  final ChildIndexGetter? findChildIndexCallback;
+
+  const FireSliverList(
+      {super.key,
+      required this.crud,
+      required this.builder,
+      this.empty = const SizedBox.shrink(),
+      this.addAutomaticKeepAlives = true,
+      this.addRepaintBoundaries = true,
+      this.addSemanticIndexes = true,
+      this.findChildIndexCallback,
+      this.semanticChildCount,
+      this.semanticIndexOffset = 0,
+      this.semanticIndexCallback = _kDefaultSemanticIndexCallback,
+      this.onViewerInit,
+      this.loading = const ListTile(),
+      this.failed = const SizedBox.shrink(),
+      this.query,
+      this.streamWindow = 50,
+      this.streamWindowPadding = 9,
+      this.memorySize = 256,
+      this.limitedSizeDoubleCheckCountThreshold = 10000,
+      this.sizeCheckInterval = const Duration(seconds: 30),
+      this.streamRetargetCooldown = const Duration(seconds: 3)});
+
+  @override
+  State<FireSliverList<T>> createState() => _FireSliverListState();
+}
+
+class _FireSliverListState<T> extends State<FireSliverList<T>> {
+  late CollectionViewer<T> viewer;
+
+  @override
+  void initState() {
+    viewer = widget.crud.view(
+        streamWindowPadding: widget.streamWindowPadding,
+        streamWindow: widget.streamWindow,
+        memorySize: widget.memorySize,
+        limitedSizeDoubleCheckCountThreshold:
+            widget.limitedSizeDoubleCheckCountThreshold,
+        sizeCheckInterval: widget.sizeCheckInterval,
+        streamRetargetCooldown: widget.streamRetargetCooldown,
+        query: widget.query);
+    widget.onViewerInit?.call(viewer);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    viewer.close();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) =>
+      viewer.stream.build((viewer) => viewer.getSize().build((size) => size == 0
+          ? widget.empty
+          : SliverList(
+              delegate: SliverChildBuilderDelegate(
+              (context, index) => viewer.getAt(index).build(
+                  (item) => item == null
+                      ? widget.failed
+                      : widget.builder(context, item),
+                  loading: widget.loading),
+              childCount: size,
+              addAutomaticKeepAlives: widget.addAutomaticKeepAlives,
+              addRepaintBoundaries: widget.addRepaintBoundaries,
+              addSemanticIndexes: widget.addSemanticIndexes,
+              findChildIndexCallback: widget.findChildIndexCallback,
+              semanticIndexCallback: widget.semanticIndexCallback,
+              semanticIndexOffset: widget.semanticIndexOffset,
+            ))));
+}
