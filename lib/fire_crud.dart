31db367ae509b124e38580bb2899e28d6285b2c7
cyberpwn <danielmillst@gmail.com>
2024-07-02 16:47:41 -0400

    Setup for 2.0.0

--- lib/fire_crud.dart
+++ lib/fire_crud.dart
@@ -1,9 +1,1 @@
 library fire_crud;
-
-export 'package:fire_crud/src/collection_viewer.dart';
-export 'package:fire_crud/src/crud.dart';
-export 'package:fire_crud/src/event.dart';
-export 'package:fire_crud/src/fire_grid.dart';
-export 'package:fire_crud/src/fire_list.dart';
-export 'package:fire_crud/src/pricing.dart';
-export 'package:fire_crud/src/sharer.dart';
--- lib/src/pricing.dart
+++
@@ -1,3 +0,0 @@
-double kFireCrudCostPerRead = 0.0345 / 100000.0;
-double kFireCrudCostPerWrite = 0.1042 / 100000.0;
-double kFireCrudCostPerDelete = 0.0115 / 100000.0;
