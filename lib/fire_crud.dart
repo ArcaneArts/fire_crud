1dd01b64d816d2aa893fd11d357a0dcdea29fbcd
cyberpwn <danielmillst@gmail.com>
2024-09-21 18:50:01 -0400

    f

--- lib/fire_crud.dart
+++ lib/fire_crud.dart
@@ -1,5 +1,7 @@
 library fire_crud;
 
+import 'package:fire_crud/src/model_crud.dart';
+
 export 'package:fire_crud/src/child_model.dart';
 export 'package:fire_crud/src/collection_view.dart';
 export 'package:fire_crud/src/fire_crud.dart';
@@ -7,6 +9,30 @@ export 'package:fire_crud/src/model_accessor.dart';
 export 'package:fire_crud/src/model_crud.dart';
 export 'package:fire_crud/src/model_utility.dart';
 
-class FireCrudGenerator {
-  const FireCrudGenerator();
+enum ModelCodec {
+  jsonMappable,
+}
+
+class Model<T extends ModelCrud> {
+  final String collection;
+  final ModelCodec codec;
+
+  const Model({required this.collection, this.codec = ModelCodec.jsonMappable});
+}
+
+class ExclusiveModel<T extends ModelCrud> extends Model<T> {
+  final String exclusiveDocumentId;
+
+  const ExclusiveModel({
+    required super.collection,
+    required this.exclusiveDocumentId,
+  });
+}
+
+class FireCrudGenerator<T extends ModelCrud> {
+  final List<Model> children;
+
+  const FireCrudGenerator({
+    this.children = const [],
+  });
 }
